eb93a33af87c7def7adaccf5cc37be7c08047e63
azertyiuop <leyiseverywhere@gmail.com>
2025-10-27 07:56:29 +0100

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -16,6 +16,7 @@ export const SERVER_CONFIG = {
   RTMP_PORT: parseInt(process.env.RTMP_PORT) || 1935,
   HTTP_PORT: parseInt(process.env.HTTP_PORT) || 8003,
 
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN || 'MTQyMzAwNjgyMjI1OTc1NzExOA.GLI6iY.OEms4nabRQ4eEsCm7eSieRDY1WYXUi8zqLppjo'
   DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL || 'https://discord.com/api/webhooks/1410403207347310592/C_OEyiIhN4lVBiDq85lXJFCuYNk-9103OjFOrq64-NY4K4P3g4e8bLc5Kvt9XaGMlET1',
 
   FRONTEND_URL: process.env.FRONTEND_URL || '*',
